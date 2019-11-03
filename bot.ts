97d2defc91ed1301c03978e717e4defe93c7216b
Nicholas Xavier <nicho@nicho.dev>
2019-11-03 13:24:21 -0800

    add vebose log to file

--- bot.ts
+++ bot.ts
@@ -25,7 +25,7 @@ const logger: Logger = winston.createLogger({
 	transports: [
 		new winston.transports.Console({ level: 'debug' }),
 		new winston.transports.File({ filename: './logs/log-error.log', level: 'error' }),
-		new winston.transports.File({ filename: './logs/log-combined.log' })
+		new winston.transports.File({ filename: './logs/log-combined.log', level: 'verbose' })
 	]
 });
 
